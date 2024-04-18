30d66aaa0e152caef37258479b80c7abe40429bb
Krit-Coder <kritraj.01@gmail.com>
2024-04-18 22:53:00 +0530

    working on it

--- OMR/main.py
+++ OMR/main.py
@@ -2,8 +2,14 @@ import cv2
 import numpy as np
 import utils
 
-path = "4.jpg"
+################################
+path = "5.jpg"
 widthImg, heightImg = 700, 700
+questions = 10
+choices = 5
+ans = [0, 2, 2, 1, 0, 2, 2, 4, 3, 1]
+################################
+
 
 # PREPROCESSING
 img = cv2.imread(path)
@@ -36,10 +42,47 @@ if biggestContour.size != 0 and gradePoints.size != 0:
     matrix = cv2.getPerspectiveTransform(p1, p2)
     imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
 
+    pg1 = np.float32(gradePoints)
+    pg2 = np.float32([[0, 0], [325, 0], [0, 150], [325, 150]])
+    matrixG = cv2.getPerspectiveTransform(pg1, pg2)
+    imgGradeDisplay = cv2.warpPerspective(img, matrixG, (325, 150))
+    # cv2.imshow("Grade", imgGradeDisplay)
+
+    # APPLY THRESHOLD
+    imgWarpGary = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)
+    imgThresh = cv2.threshold(imgWarpGary, 170, 255, cv2.THRESH_BINARY_INV)[1]
+
+    boxes = utils.splitBoxes(imgThresh)
+    # cv2.imshow("Boxes:", boxes[0])
+    # cv2.imshow("Boxes:", boxes[2])
+
+    # print(cv2.countNonZero(boxes[0]), cv2.countNonZero(boxes[1]), cv2.countNonZero(boxes[2]), cv2.countNonZero(
+    #     boxes[3]), cv2.countNonZero(boxes[4]))
+
+    # GETTING NON ZERO PIXEL VALUES OF EACH BOX
+    myPixelVal = np.zeros((questions, choices))
+    countC = 0
+    countR = 0
+    for image in boxes:
+        totalPixels = cv2.countNonZero(image)
+        myPixelVal[countR][countC] = totalPixels
+        countC += 1
+        if countC == choices:
+            countR += 1
+            countC = 0
+
+    # print(myPixelVal)
+
+    myIndex = []
+    for x in range(questions):
+        arr = myPixelVal[x]
+        myIndexVal = np.where(arr == np.amax(arr))
+        # print(myIndexVal[0])
+        myIndex.append(myIndexVal[0][0])
 
 imgBlank = np.zeros_like(img)
 imageArray = ([img, imgGray, imgBlur, imgCanny],
-              [imgContours, imgBiggestContours, imgWarpColored, imgBlank])
+              [imgContours, imgBiggestContours, imgWarpColored, imgThresh])
 imgStacked = utils.stackImages(imageArray, 0.5)
 
 cv2.imshow("Stacked Images", imgStacked)
